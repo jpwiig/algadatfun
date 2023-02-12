eaec782fece04581de140304f83512615e5a247a
jpwiig <jpwiig@gmail.com>
2023-02-12 13:26:06 +0100

    2021  - ver 2

--- src/main.java
+++ src/main.java
@@ -1,9 +1,5 @@
-import Algoritmereffektivitet.Tabell;
 import Algoritmereffektivitet.fakultet;
 import Algoritmereffektivitet.maksogmin;
-import Algoritmereffektivitet.tid;
-
-import java.util.Arrays;
 
 public class main {
     public static void main(String[] args) {
